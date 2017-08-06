78a421fc718fc51bfe1587a2deaf7257fcbd753b
vckadam <vckadam@asu.edu>
2017-08-06 02:35:10 -0400

    Added test cases for getBookByLocation bookStore

--- test/com/vckadam/oopdesign/bookstore/test/ServiceImplTest.java
+++ test/com/vckadam/oopdesign/bookstore/test/ServiceImplTest.java
@@ -47,6 +47,116 @@ public class ServiceImplTest {
 		assertEquals(expectedBkToLocMap, actualBkToLocsMap);
 	}
 	
+	@Test
+	public void testGetBooksByLocation_BookWithoutLoc() {
+		Long[] bookIds = {1L ,2L,3L, 4L}; //4L without location
+		List<Book> books = prepBooksForTestGetBooksByLocation(bookIds);
+		long[][] locToBookIds = {{1,1},{2,1},{3,1},{1,2},{3,3}};
+		List<BookLocation> locs = prepLocsForTestGetBooksByLocation(locToBookIds);
+		List<LocsByBook> locsByBook = this.service.getBooksByLocation(books, locs);
+		Map<Long,List<Long>> actualBkToLocsMap = prepActualBkToLocsMap(locsByBook);
+		
+		Object[][] expectedBkToLocs = {{1L,Arrays.asList(1L,2L,3L)},{2L,Arrays.asList(1L)},{3L,Arrays.asList(3L)}};
+		Map<Long,List<Long>> expectedBkToLocMap = prepExpectedBkToLocsMap(expectedBkToLocs);
+		
+		assertEquals(expectedBkToLocMap.size(), locsByBook.size());
+		assertEquals(expectedBkToLocMap, actualBkToLocsMap);
+	}
+	
+	@Test
+	public void testGetBooksByLocation_LocIdWithInvalidBookId() {
+		Long[] bookIds = {1L ,2L,3L, 4L}; //4L without location
+		List<Book> books = prepBooksForTestGetBooksByLocation(bookIds);
+		long[][] locToBookIds = {{1,1},{2,1},{3,1},{1,2},{3,3},{3,6}}; //bookId with 6 doen't exist.
+		List<BookLocation> locs = prepLocsForTestGetBooksByLocation(locToBookIds);
+		List<LocsByBook> locsByBook = this.service.getBooksByLocation(books, locs);
+		Map<Long,List<Long>> actualBkToLocsMap = prepActualBkToLocsMap(locsByBook);
+		
+		Object[][] expectedBkToLocs = {{1L,Arrays.asList(1L,2L,3L)},{2L,Arrays.asList(1L)},{3L,Arrays.asList(3L)}};
+		Map<Long,List<Long>> expectedBkToLocMap = prepExpectedBkToLocsMap(expectedBkToLocs);
+		
+		assertEquals(expectedBkToLocMap.size(), locsByBook.size());
+		assertEquals(expectedBkToLocMap, actualBkToLocsMap);
+	}
+	
+	@Test
+	public void testGetBooksByLocation_DuplicateBookId() {
+		Long[] bookIds = {1L ,2L,3L, 3L}; //3L duplicate book Id 
+		List<Book> books = prepBooksForTestGetBooksByLocation(bookIds);
+		long[][] locToBookIds = {{1,1},{2,1},{3,1},{1,2},{3,3}};
+		List<BookLocation> locs = prepLocsForTestGetBooksByLocation(locToBookIds);
+		List<LocsByBook> locsByBook = this.service.getBooksByLocation(books, locs);
+		Map<Long,List<Long>> actualBkToLocsMap = prepActualBkToLocsMap(locsByBook);
+		
+		Object[][] expectedBkToLocs = {{1L,Arrays.asList(1L,2L,3L)},{2L,Arrays.asList(1L)},{3L,Arrays.asList(3L)}};
+		Map<Long,List<Long>> expectedBkToLocMap = prepExpectedBkToLocsMap(expectedBkToLocs);
+		
+		assertEquals(expectedBkToLocMap.size(), locsByBook.size());
+		assertEquals(expectedBkToLocMap, actualBkToLocsMap);
+	}
+	
+	@Test
+	public void testGetBooksByLocation_DuplicateLocToBookId() {
+		Long[] bookIds = {1L ,2L,3L, 3L}; //3L duplicate book Id 
+		List<Book> books = prepBooksForTestGetBooksByLocation(bookIds);
+		long[][] locToBookIds = {{1,1},{2,1},{3,1},{1,2},{3,3},{3,3}}; //3,3 locTobookId duplicate
+		List<BookLocation> locs = prepLocsForTestGetBooksByLocation(locToBookIds);
+		List<LocsByBook> locsByBook = this.service.getBooksByLocation(books, locs);
+		Map<Long,List<Long>> actualBkToLocsMap = prepActualBkToLocsMap(locsByBook);
+		
+		Object[][] expectedBkToLocs = {{1L,Arrays.asList(1L,2L,3L)},{2L,Arrays.asList(1L)},{3L,Arrays.asList(3L)}};
+		Map<Long,List<Long>> expectedBkToLocMap = prepExpectedBkToLocsMap(expectedBkToLocs);
+		
+		assertEquals(expectedBkToLocMap.size(), locsByBook.size());
+		assertEquals(expectedBkToLocMap, actualBkToLocsMap);
+	}
+	
+	@Test
+	public void testGetBooksByLocation_NullBookId() {
+		Long[] bookIds = {null,1L, null,2L,null,3L}; 
+		List<Book> books = prepBooksForTestGetBooksByLocation(bookIds);
+		long[][] locToBookIds = {{1,1},{2,1},{3,1},{1,2},{3,3}};
+		List<BookLocation> locs = prepLocsForTestGetBooksByLocation(locToBookIds);
+		List<LocsByBook> locsByBook = this.service.getBooksByLocation(books, locs);
+		Map<Long,List<Long>> actualBkToLocsMap = prepActualBkToLocsMap(locsByBook);
+		
+		Object[][] expectedBkToLocs = {{1L,Arrays.asList(1L,2L,3L)},{2L,Arrays.asList(1L)},{3L,Arrays.asList(3L)}};
+		Map<Long,List<Long>> expectedBkToLocMap = prepExpectedBkToLocsMap(expectedBkToLocs);
+		
+		assertEquals(expectedBkToLocMap.size(), locsByBook.size());
+		assertEquals(expectedBkToLocMap, actualBkToLocsMap);
+	}
+	
+	@Test
+	public void testGetBooksByLocation_NullLocToBookId() {
+		Long[] bookIds = {null,1L, null,2L,null,3L}; 
+		List<Book> books = prepBooksForTestGetBooksByLocation(bookIds);
+		long[][] locToBookIds = {null,{1,1},{2,1},null,{3,1},null,{1,2},{3,3}};
+		List<BookLocation> locs = prepLocsForTestGetBooksByLocation(locToBookIds);
+		List<LocsByBook> locsByBook = this.service.getBooksByLocation(books, locs);
+		Map<Long,List<Long>> actualBkToLocsMap = prepActualBkToLocsMap(locsByBook);
+		
+		Object[][] expectedBkToLocs = {{1L,Arrays.asList(1L,2L,3L)},{2L,Arrays.asList(1L)},{3L,Arrays.asList(3L)}};
+		Map<Long,List<Long>> expectedBkToLocMap = prepExpectedBkToLocsMap(expectedBkToLocs);
+		
+		assertEquals(expectedBkToLocMap.size(), locsByBook.size());
+		assertEquals(expectedBkToLocMap, actualBkToLocsMap);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetBooksByLocation_NullBookList() {
+		List<Book> books = null;
+		long[][] locToBookIds = {null,{1,1},{2,1},null,{3,1},null,{1,2},{3,3}};
+		List<BookLocation> locs = prepLocsForTestGetBooksByLocation(locToBookIds);
+		this.service.getBooksByLocation(books, locs);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetBooksByLocation_NullLocList() {
+		List<Book> books = null;
+		List<BookLocation> locs = null;
+		this.service.getBooksByLocation(books, locs);
+	}
 	
 	private List<Book> prepBooksForTestGetBooksByLocation(Long[] bookIds) {
 		List<Book> books = new ArrayList<>();
@@ -60,7 +170,7 @@ public class ServiceImplTest {
 	private List<BookLocation> prepLocsForTestGetBooksByLocation(long[][] locToBookIds) {
 		List<BookLocation> bkLocs = new ArrayList<>();
 		for(long[] locToBookId : locToBookIds){
-			if(locToBookIds != null) {
+			if(locToBookId != null) {
 				bkLocs.add(new BookLocation(locToBookId[0], null, locToBookId[1], 0));
 			} else bkLocs.add(null);
 		}
